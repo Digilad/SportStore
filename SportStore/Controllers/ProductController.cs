1551fe25c20a3eb8a2c966338d8a5ada7b56e997
Digilad <digilad@gmail.com>
2018-03-16 01:52:10 +0200

    First commit

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
